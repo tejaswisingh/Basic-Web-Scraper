f0ba1450c2431ca73335a038bb793df6e2f33dd7
TEJASWI SINGH <tejaswisingh14@gmail.com>
2021-09-29 06:33:27 -0400

    Initial Push

---
+++ index.js
@@ -0,0 +1,28 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const {response} = require("express");
+
+const app = express();
+const url = 'https://enter the url of the website to be scraped here';
+
+axios(url)
+    .then(response => {
+        const articles = [];
+        const html = response.data;  // Literally returns back all of the html of the url.
+        const $ = cheerio.load(html); // Assigning cheerio.load to a variable for cherry picking of html elements.
+        $('.item__title', html).each(function() { // <-- cannot be a function expression
+            const title = $(this).text();
+            // $(this).attr('href'); <-- You can either directly call it like this or follow the syntax below
+            const url = $(this).find('a').attr('href');
+            articles.push({
+                title,
+                url
+            });
+        })
+        //console.log(html);
+        console.log(articles);
+    }).catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Basic Web Scraper Server running on PORT ${PORT}`));
